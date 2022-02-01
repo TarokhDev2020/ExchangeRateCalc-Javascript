7d1c8345f7d934233b6d12c44918246f7b33f7d9
Tarokh Mohammadi <tarokhDevMoh2020@gmail.com>
2022-02-01 19:25:57 +0330

    main commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const firstCurrencyEl = document.getElementById("currency-one");
+const secondCurrencyEl = document.getElementById("currency-two");
+const firstAmount = document.getElementById("amount-one");
+const secondAmount = document.getElementById("amount-two");
+const swapButton = document.getElementById("swap");
+const rateEl = document.getElementById("rate");
+
+function calculate() {
+    const firstCurrency = firstCurrencyEl.value;
+    const secondCurrency = secondCurrencyEl.value;
+    fetch(`https://v6.exchangerate-api.com/v6/f17489acd9227419dff53fcd/latest/${firstCurrency}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.conversion_rates[secondCurrency];
+            rateEl.innerText = `1 ${firstCurrency} = ${rate} ${secondCurrency}`
+            secondAmount.value = (firstAmount.value * rate).toFixed(2);
+        });
+}
+
+firstCurrencyEl.addEventListener("change", calculate);
+firstAmount.addEventListener("input", calculate);
+secondCurrencyEl.addEventListener("change", calculate);
+secondAmount.addEventListener("input", calculate);
+swapButton.addEventListener("click", e => {
+    const temp = firstCurrencyEl.value;
+    firstCurrencyEl.value = secondCurrencyEl.value;
+    secondCurrencyEl.value = temp;
+    calculate();
+})
+
+calculate();
